708412e871a8eab12c89c29fa3eab80ab25432cb
xxyhxydx <137157679+xxyhxydx@users.noreply.github.com>
2024-06-07 16:24:34 +0800

    Update udpclient.py（中期）

--- udpclient.py
+++ udpclient.py
@@ -6,7 +6,9 @@ import statistics
 limit_text = 100  # 文本最大长度，不超过900
 limit_data = 1024  #最大接受数据量
 limit_socket_time = 0.1  #设置套接字超时值100ms
-udp_request_packet = 12  #request报文
+
+
+#request报文
 
 
 # udp报文封装
@@ -35,16 +37,16 @@ def tcp_connect(dest_addr):
         syn_packet = b'SYN'
         udpClient.sendto(syn_packet, dest_addr)
         # 接收SYN-ACK包
-        syn_ack,addr= udpClient.recvfrom(1024)
+        syn_ack, addr = udpClient.recvfrom(1024)
         if syn_ack == b'SYN-ACK':
-            print(dest_addr,"server is ready!")
+            print(dest_addr, "server is ready!")
             # 发送ACK包
         ack_packet = b'ACK'
         udpClient.sendto(ack_packet, dest_addr)
+        print("直接回车则结束通信！")
     except:
         print("connect refused error!")
         exit(0)
-
 
 
 # 模拟TCP连接的释放
@@ -53,11 +55,11 @@ def tcp_disconnect(dest_addr):
     fin_packet = b'FIN'
     udpClient.sendto(fin_packet, dest_addr)
     # 接收ACK包
-    ack,addr = udpClient.recvfrom(1024)
+    ack, addr = udpClient.recvfrom(1024)
     if ack == b'ACK':
-        print(dest_addr,"bye!")
+        print(dest_addr, "bye!")
     # 接收FIN-ACK包
-    fin_ack,addr= udpClient.recvfrom(1024)
+    fin_ack, addr = udpClient.recvfrom(1024)
     if fin_ack == b'FIN-ACK':
         # 发送ACK包
         fin_packet = b'ACK'
@@ -76,27 +78,31 @@ udpClient.settimeout(limit_socket_time)  #设置超时
 tcp_connect(serverAddr)
 
 #通信
-success_udp_packets = 0
-sum_udp_packets = 0
+udp_receive_packets = 0
+udp_request_packets = 0
 rtt_list = []
 server_time_list = []
-for i in range(udp_request_packet):
-    i += 1  #分组号
-    text=''
+
+while True:
+    text = ''
     while True:
         text = input("输入你想发送的信息：")
-        if len(text)<limit_text: break
-        else: print("文本超出限制！")
-    data = pack(i, "2", text)  # 报文数据，bytes类型
+        if len(text) < limit_text:
+            break
+        else:
+            print("文本超出限制！")
+    if len(text) == 0:
+        break
+    data = pack(udp_request_packets + 1, "2", text)  # 报文数据，bytes类型
     again = 0  #超时重传机会计数
     while again < 3:
+        udp_request_packets += 1
         try:
             sendTime = time.time()
-            sum_udp_packets += 1
             udpClient.sendto(data, serverAddr)  # 发送数据给服务端
             serverData, serverAddr = udpClient.recvfrom(limit_data)  # 接收来自服务端的数据
             server_time_list.append(serverData[10:18].decode('utf-8'))
-            success_udp_packets += 1
+            udp_receive_packets += 1
             rtt = time.time() - sendTime
             rtt_list.append(rtt)
             print("seq_no：", data[0:8].decode('utf_8'), " rtt：", rtt)
@@ -107,13 +113,19 @@ for i in range(udp_request_packet):
             again += 1
 
 # 通信反馈
-print("receive udp packets：", success_udp_packets)
-print("packet loss rate：", 1 - (success_udp_packets / sum_udp_packets))
+print("receive udp packets：", udp_receive_packets)
+if udp_request_packets==0:
+    print("packet loss rate：no  udp_request_packets")
+    print("max rtt：0", "min rtt：0", "mean rtt：0","stdev rtt：0")
+    print("server overall responsiveness：0")
+else:
+    print("packet loss rate：", 1 - (udp_receive_packets / udp_request_packets))
 print("max rtt：", max(rtt_list), "min rtt：", min(rtt_list), "mean rtt：", sum(rtt_list) / len(rtt_list), "stdev rtt：",
       statistics.stdev(rtt_list))
-t1 = datetime.datetime.strptime(server_time_list[4], '%H:%M:%S')
+t1 = datetime.datetime.strptime(server_time_list[udp_receive_packets -1], '%H:%M:%S')
 t2 = datetime.datetime.strptime(server_time_list[0], '%H:%M:%S')
 print("server overall responsiveness：", t1 - t2)
+
 # 模拟TCP连接的释放
 tcp_disconnect(serverAddr)
 
